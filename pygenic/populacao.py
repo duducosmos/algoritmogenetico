35a45157652ca15e47bd5a081a968b9c8b61d1e0
Eduardo Pereira <pereira.somoza@gmail.com>
2019-01-22 23:39:52 -0200

    Primeiro Commit

---
+++ pygenic/populacao.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3.6
+# -*- Coding: UTF-8 -*-
+"""
+Gerador aleatório de população.
+
+Programa sob licença GNU V.3.
+Desenvolvido por: E. S. Pereira.
+Versão 0.0.1.
+""""
+
+from numpy import randint
+
+class Populacao:
+    """
+    Cria e avalia uma população.
+    Recebe como entrada:
+        avaliacao - Função que recebe um indivíduo como entrada e retorna
+                    um valor numérico.
+        cromossos_totais - Numero inteiro representando o tamanho da cadeia
+                           genética do indivíduo.
+        tamanho_populacao - Numero inteiro representando o número total de
+                            indivíduos na população.
+    """
+
+    def __init__(self, avaliacao, cromossos_totais, tamanho_populacao):
+        self.avaliacao = avaliacao
+        self.cromossos_totais = cromossos_totais
+        self.tamanho_populacao = tamanho_populacao
+
+    def gerar_populacao(self):
+        """Gerador aleatório de população."""
+
+        self.populacao = randint(0, 2, size=(self.cromossos_totais,
+                                             self.tamanho_populacao
+                                             ), dtype='b')
